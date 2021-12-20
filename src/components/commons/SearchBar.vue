897d2fdddb972e304d71a3f30df26fb899a25f15
Andrea <57769818+andrea-nuzzo@users.noreply.github.com>
2021-12-20 16:39:58 +0100

    api functioning test

--- src/components/commons/SearchBar.vue
+++ src/components/commons/SearchBar.vue
@@ -1,14 +1,32 @@
 <template>
   <div>
       <input type="text" placeholder="Search">
-      <button>Search</button>
+      <button @click="searchFilm()">Search</button>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
     name: 'SearchBar',
 
+    methods: {
+        // Questa funzione importa il testo scritto dall'utente per cercare il film
+        searchFilm(){
+            axios.get('https://api.themoviedb.org/3/search/movie', {
+                params: {
+                api_key: '012221f6f2f19b76150fb8c79d712a76',
+                query: 'ritorno al futuro'
+                }
+            })
+            .then(function (response) {
+                console.log(response.data.results);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+        }
+    },
 }
 </script>
 
